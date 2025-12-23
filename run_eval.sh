bb7633d0ebbf6f441d6b8454b72b016714effb2e
shimeming <shimeming@gmail.com>
2025-12-23 13:17:09 +0800

    remove run scripts

--- run_eval.sh
+++
@@ -1,6 +0,0 @@
-uv run -m classeval_eval.generate \
-  --config configs/3b_lora_classeval.yaml \
-  --eval_output_dir classeval_eval/results/our_eval_output \
-  --greedy 0 \
-  --lora_weights checkpoints/checkpoint-105
-  # --test_work 1 \
